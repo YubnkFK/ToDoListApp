36acb92418552478946ad84fc33a35e04c71beb4
Michael Yubank <michael.yubank@factork-software.com>
2025-06-11 13:14:56 -0600

    refactor index

--- index.js
+++ index.js
@@ -8,9 +8,4 @@ import { name as appName } from './app.json';
 
 let AppEntryPoint = App;
 
-if (process.env.STORYBOOK_ENABLED === 'true') {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    AppEntryPoint = require('./.storybook').default;
-}
-
 AppRegistry.registerComponent(appName, () => AppEntryPoint);
